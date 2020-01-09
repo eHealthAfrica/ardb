af6a24166e40e0eb80ea2952c8a155e137179aa0
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2020-01-09 15:27:15 +0100

    fix: allow for REDIS_PASSWORD to be none

--- ardb/start.sh
+++ ardb/start.sh
@@ -10,7 +10,19 @@ export KEEP_ALIVE=${KEEP_ALIVE:=60}
 export LOG_LEVEL=${LOG_LEVEL:=error}
 export LOG_FILE=${LOG_FILE:=stdout}
 export DATA_DIR=${DATA_DIR:=../data}
-export REDIS_PASSWORD=${REDIS_PASSWORD:=password}
 
-# Start database
+# Apply Password if required
+if [[ ! -v REDIS_PASSWORD ]]; then
+    echo "no password"
+    cp $ARDB_CONF ./ardb.tmp
+    sed -i "/requirepass/c\#requirepass" ardb.tmp
+    cp ./ardb.tmp $ARDB_CONF 
+else
+    echo "password"
+    cp $ARDB_CONF ./ardb.tmp
+    sed -i "/requirepass/c\requirepass $REDIS_PASSWORD" ardb.tmp
+    cp ./ardb.tmp $ARDB_CONF
+fi
+
+# Start Database
 /opt/ardb/ardb-server ${ARDB_CONF}
